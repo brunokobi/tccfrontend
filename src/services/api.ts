5d74459b89c790dc3eb49e3a18f329dbbec10f4f
brunokobi <38856838+brunokobi@users.noreply.github.com>
2020-11-02 23:28:18 -0300

    env heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://radiant-gorge-04331.herokuapp.com",
 });
 
 export default api;
